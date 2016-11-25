c20c50ae6cc0357ff4e03177f641ffc1b24fde2a
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-11-25 01:29:17 +0100

    Add Role model and migration

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = ['admin', 'user'];
+
+        foreach ($roles as $role) {
+            DB::table('roles')->insert([
+                'name' => $role,
+            ]);
+        }
+    }
+}
