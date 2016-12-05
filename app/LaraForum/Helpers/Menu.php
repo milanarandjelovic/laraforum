a2c1c41b5e572d05a30469092c01ffca308a20f3
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-12-05 05:49:11 +0100

    Added helper class for forum menu

--- app/LaraForum/Helpers/Menu.php
+++ app/LaraForum/Helpers/Menu.php
@@ -7,12 +7,23 @@ use Request;
 class Menu
 {
 
-    public static function activeMenu($uri = '')
+    public static function activeAdminMenu($uri = '')
     {
         $active = '';
 
         if (Request::is(Request::segment(1) . '/' . $uri . '/*') || Request::is(Request::segment(1) . '/' . $uri) || Request::is($uri)) {
             $active = 'active';
+        }
+
+        return $active;
+    }
+
+    public static function activeForumMenu($uri = '')
+    {
+        $active = '';
+
+        if (Request::is(Request::segment(1) . '/' . $uri . '/*') || Request::is(Request::segment(1) . '/' . $uri) || Request::is($uri)) {
+            $active = 'is-active';
         }
 
         return $active;
--- config/app.php
+++ config/app.php
@@ -230,7 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'HelpMenu' => App\LaraForum\Helpers\Menu::class,
     ],
 
 ];
