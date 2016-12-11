59a9110303fe8ed969d7bf458c362e2eafd51db1
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-12-11 15:21:23 +0100

    Added Pagination vue component

---
+++ resources/assets/js/vuejs/components/common/Pagination.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="pagination">
+
+    <ul class="pagination" :class="navClass">
+      <li class="footable-page-arrow" v-if="pagination.current_page > 1">
+        <a data-page="first" @click.prevent="changePage(1)">«</a>
+      </li>
+      <li class="footable-page-arrow" v-if="pagination.current_page > 1">
+        <a data-page="prev" @click.prevent="changePage(pagination.current_page - 1)">‹</a>
+      </li>
+      <li v-for="page in pagination.per_page"
+          class="footable-page"
+          :class="page == pagination.current_page ? 'active': ''"
+      >
+        <a @click.prevent="changePage(page)">{{ page }}</a>
+      </li>
+      <li class="footable-page-arrow" v-if="pagination.current_page < pagination.total_page">
+        <a data-page="next" @click.prevent="changePage(pagination.current_page + 1)">›</a>
+      </li>
+      <li class="footable-page-arrow" v-if="pagination.current_page < pagination.total_page">
+        <a data-page="last" @click.prevent="changePage(pagination.total.page)">»</a>
+      </li>
+    </ul>
+
+  </div>
+</template>
+<script>
+  export default{
+    name: 'pagination',
+    data(){
+      return{
+      }
+    },
+
+     props: {
+        pagination: {
+          type: Object,
+          required: true
+        },
+        callback: {
+          type: Function,
+          required: true
+        },
+        size: {
+          type: String,
+          default: ""
+        },
+        navClass: {
+          type: String,
+          default: ""
+        },
+        offset: {
+          type: Number,
+          default: 4
+        },
+        visible: {
+          type: Number,
+          default: 1
+        }
+    }, // props
+
+    computed: {
+      data () {
+        this.visible = 1
+
+        var from = this.pagination.current_page - this.offset
+        if(from < 1) {
+          from = 1
+        }
+
+        var to = from + (this.offset * 2)
+        if(to >= this.pagination.total_pages) {
+          to = this.pagination.total_pages
+        }
+        this.from = from
+        this.to = to
+
+        var arr = []
+        while (from <=to) {
+          arr.push(from)
+          from++
+        }
+
+        if(arr.length == 1) {
+          this.visible = 0
+        }
+
+        return arr
+      }
+    }, // computed
+
+    watch: {
+      'pagination.per_page': () => {
+      // this.callback()
+      }
+    }, // watch
+
+    methods: {
+      changePage (page) {
+        this.pagination.current_page = page
+        this.callback(page)
+      }
+    } // methods
+  }
+</script>
