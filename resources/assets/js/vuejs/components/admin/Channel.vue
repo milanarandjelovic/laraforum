a8a6f66dc36a287773c31dd5885ca44e0441c963
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-12-11 16:22:18 +0100

    Fix css class

--- resources/assets/js/vuejs/components/admin/Channel.vue
+++ resources/assets/js/vuejs/components/admin/Channel.vue
@@ -165,10 +165,10 @@
             <td>{{ channel.created_at }}</td>
             <td>{{ channel.updated_at }}</td>
             <td>
-              <button class="btn btn-white btn-sm" @click.prevent="openUpdateChannelModal(channel.id)">
+              <button class="btn btn-white btn-xs" @click.prevent="openUpdateChannelModal(channel.id)">
                 <i class="fa fa-pencil"></i> Edit
               </button>
-              <button class="btn btn-danger btn-sm" @click.prevent="deleteChannel(channel.id)">
+              <button class="btn btn-danger btn-xs" @click.prevent="deleteChannel(channel.id)">
                 <i class="fa fa-trash-o"></i> Deleted
               </button>
             </td>
