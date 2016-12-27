988ead0dbe8d0dc69d2c93240e1d7c859c36d819
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-12-27 17:43:47 +0100

    Fix scss

--- resources/views/forum/channels/index.blade.php
+++ resources/views/forum/channels/index.blade.php
@@ -20,19 +20,26 @@
                     @foreach($discussions as $discussion)
                         <div class="forum-item">
                             <div class="row">
-                                <div class="col-md-1 col-sm-1 col-xs-1 forum-info">
+                                <div class="col-md-1 col-sm-1 col-xs-1 forum-channel-creator-avatar">
                                     <a href="/{{ '@' .$discussion->username }}">
                                         <img src="{{ Gravatar::src($discussion->email, 38) }}" class="img img-circle">
                                     </a>
                                 </div> {{-- /.col-md-1 --}}
                                 <div class="col-md-10 col-sm-10 col-xs-10">
                                     <div class="form-discuss-links">
-                                        <a href="/discuss/channels/" class="forum-item-title">{{ $discussion->title }}</a>
-                                        <a href="{{ $discussion->channel_url }}">{{ strtoupper($discussion->name) }}</a>
-                                        <a href="{{ $discussion->channel_url }}">
-                                            {{ strtoupper(\Carbon\Carbon::createFromTimeStamp(strtotime($discussion->created_at))->diffForHumans()) }}
+                                        <a href="/discuss/channels/" class="forum-item-title">
+                                            {{ $discussion->title }}
                                         </a>
-                                        BY <a href="">{{ strtoupper($discussion->username) }}</a>
+                                        <a href="{{ $discussion->channel_url }}" class="form-channel-name">
+                                            {{ strtoupper($discussion->name) }}
+                                        </a>
+                                        <a href="{{ $discussion->channel_url }}" class="forum-channel-date">
+                                            <span>•</span> {{ strtoupper(\Carbon\Carbon::createFromTimeStamp(strtotime($discussion->created_at))->diffForHumans()) }}
+                                            BY
+                                        </a>
+                                        <a href="/{{ '@' .$discussion->username }}" class="forum-channel-creator">
+                                            {{ strtoupper($discussion->username) }}
+                                        </a>
                                     </div> {{-- /.forum-discuss-links --}}
                                     <div class="forum-discuss-description">
                                         {{ substr($discussion->description, 0, 160) . '...'}}
@@ -47,7 +54,9 @@
                             </div> {{-- /.row --}}
                         </div> {{-- /.forum-item --}}
                     @endforeach
-                    {{ $discussions->links() }}
+                    <div class="pagination-holder">
+                        {{ $discussions->links() }}
+                    </div>
                 @else
                     <div class="alert alert-info mt-15">
                         <strong>No discussion for this channel.</strong>
