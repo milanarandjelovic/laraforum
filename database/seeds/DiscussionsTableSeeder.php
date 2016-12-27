ae4b6d56d6b3c42409f63b2c2f2f13f4f3101a09
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-12-27 03:15:03 +0100

    Added DiscussionsTableSeeder

---
+++ database/seeds/DiscussionsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DiscussionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Models\Discussion::class, 30)->create();
+    }
+}
