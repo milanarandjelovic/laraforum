d1e668a7afb0e0b439eabc337aa2ad22b6cea9f2
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-12-27 21:34:28 +0100

    Fix color to page heading

--- resources/views/forum/channels/partials/page-heading.blade.php
+++ resources/views/forum/channels/partials/page-heading.blade.php
@@ -1,5 +1,5 @@
 <?php $num_segment = count(Request::segments()) ?>
-<ol class="breadcrumb">
+<ol class="breadcrumb" style="background-color: #F9F9F9">
     @foreach(\Request::segments() as $key => $segment)
         @if($key === 0 || $key === 1)
             <li>
