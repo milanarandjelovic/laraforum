c95b6db13859462b01e45476948666168d825efc
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2017-01-20 05:46:46 +0100

    Fix

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -47,7 +47,7 @@
 
                     <div class="row">
                         <div class="col-lg-12">
-                            <div class="wrapper wrapper-content animated fadeInRight">
+                            <div class="wrapper wrapper-content">
                                 <div class="row">
                                     @yield('content')
                                 </div> {{-- /.row --}}
