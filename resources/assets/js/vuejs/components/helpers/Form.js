78cffec21fa96b963b4edb3492120232be6c8db1
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2017-01-30 15:37:09 +0100

    Add helper for vue form

---
+++ resources/assets/js/vuejs/components/helpers/Form.js
@@ -0,0 +1,156 @@
+import axios from 'axios';
+import Errors from './Errors';
+
+class Form {
+
+  /**
+   * Create a new Form instance.
+   *
+   * @param {object} data
+   */
+  constructor(data) {
+
+    if (Array.isArray(data)) {
+      data = data.reduce((carry, element) => {
+        carry[element] = '';
+        return carry;
+      }, {});
+    }
+
+    this.originalData = data;
+    this.errors = new Errors();
+
+    for (const field in data) {
+      this[field] = data[field];
+    }
+  }
+
+  /**
+   * Fetch all relevant data for the form.
+   */
+  data() {
+    const data = {};
+
+    for (const property in this.originalData) {
+      data[property] = this[property];
+    }
+
+    return data;
+  }
+
+  /**
+   * Reset the form fields.
+   */
+  reset() {
+    for (const field in this.originalData) {
+      this[field] = this.originalData[field];
+    }
+
+    this.errors.clear();
+  }
+
+  /**
+   * Clear the form fields.
+   */
+  clear() {
+    for (const field in this.originalData) {
+      this[field] = '';
+    }
+
+    this.errors.clear();
+  }
+
+  /**
+   * Send a POST request to the given URL.
+   *
+   * @param {string} url
+   */
+  post(url) {
+    return this.submit('post', url);
+  }
+
+  /**
+   * Send a PUT request to the given URL.
+   *
+   * @param {string} url
+   */
+  put(url) {
+    return this.submit('put', url);
+  }
+
+  /**
+   * Send a PATCH request to the given URL.
+   *
+   * @param {string} url
+   */
+  patch(url) {
+    return this.submit('patch', url);
+  }
+
+  /**
+   * Send a DELETE request to the given URL.
+   *
+   * @param {string} url
+   */
+  delete(url) {
+    return this.submit('delete', url);
+  }
+
+  /**
+   * Submit the form.
+   *
+   * @param {string} requestType
+   * @param {string} url
+   */
+  submit(requestType, url) {
+    this.validateRequestType(requestType);
+
+    return new Promise((resolve, reject) => {
+      axios[requestType](url, this.data())
+        .then((response) => {
+          this.onSuccess(response.data);
+
+          resolve(response.data);
+        })
+        .catch((error) => {
+          this.onFail(error.response.data);
+
+          reject(error.response);
+        });
+    });
+  }
+
+  /**
+   * Validate a request type.
+   *
+   * @param {string} requestType
+   */
+  validateRequestType(requestType) {
+    const requestTypes = ['get', 'delete', 'head', 'post', 'put', 'patch'];
+
+    if (requestTypes.indexOf(requestType) === -1) {
+      throw new Error(`\`${requestType}\` is not a valid request type, ` +
+        `must be one of: \`${requestTypes.join('\`, \`')}\`.`);
+    }
+  }
+
+  /**
+   * Handle a successful form submission.
+   *
+   * @param {object} data
+   */
+  onSuccess() {
+    this.reset();
+  }
+
+  /**
+   * Handle a failed form submission.
+   *
+   * @param {object} errors
+   */
+  onFail(errors) {
+    this.errors.record(errors);
+  }
+}
+
+export default Form;
