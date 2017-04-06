8a0ed06501c1ad74f4fdcbe595d72f6765ae481c
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2017-04-06 17:45:10 +0200

    Add new method

--- app/LaraForum/Repositories/ChannelRepository.php
+++ app/LaraForum/Repositories/ChannelRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\LaraForum\Repositories;
 
+use DB;
 use App\Models\Channel;
 use App\LaraForum\Repositories\Eloquent\Repository;
 
@@ -16,5 +17,24 @@ class ChannelRepository extends Repository
     public function model()
     {
         return Channel::class;
+    }
+
+    /**
+     * Return all channels with discussion count.
+     *
+     * @return mixed
+     */
+    public function getAllChannelsWithDiscussions()
+    {
+        return $this->model
+            ->orderBy('name', 'asc')
+            ->join('discussions', 'channels.id', '=', 'discussions.channel_id')
+            ->select(
+                'channels.id as id', 'channels.name as name', 'channels.description as description',
+                'channels.channel_icon as channel_icon', 'channels.channel_url as channel_url',
+                DB::raw("count(discussions.channel_id) as discussion_count")
+            )
+            ->groupBy('channels.id')
+            ->get();
     }
 }
